622e1e3797b900aff204844f31bb172a33f13d01
TafadzwaG <60160165+TafadzwaG@users.noreply.github.com>
2023-11-16 12:51:43 +0200

    cloud api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const API = "http://127.0.0.1:8000/api"
+export const API = "https://king-prawn-app-jwnvi.ondigitalocean.app/api"
 
-export const IMAGE_URL = "http://127.0.0.1:8000/public/images"
+export const IMAGE_URL = "https://king-prawn-app-jwnvi.ondigitalocean.app/public/images"
 
 
 
