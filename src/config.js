200fe1b76cd7a3a532c1ec64d919ab7f3fa377bf
Tafadzwa Gashira <tafadzwagashirah@gmail.com>
2024-07-18 08:12:39 +0200

    added the api route

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const API = "http://127.0.0.1:8000/api"
+// export const API = "http://127.0.0.1:8000/api"
 
-export const IMAGE_URL = "http://127.0.0.1:8000/images"
+// export const IMAGE_URL = "http://127.0.0.1:8000/images"
 
 
 // export const API = "http://192.168.0.20:8080/update/public/api"
@@ -8,11 +8,11 @@ export const IMAGE_URL = "http://127.0.0.1:8000/images"
 // export const IMAGE_URL = "http://192.168.0.20:8080/update/public/images"
 
 
+// https://providencehumancapital.com/medicalsapi/public/api/user
 
+export const API = "https://providencehumancapital.com/medicalsapi/public/api"
 
-// export const API = "https://king-prawn-app-jwnvi.ondigitalocean.app/api"
-
-// export const IMAGE_URL = "https://king-prawn-app-jwnvi.ondigitalocean.app/public/images"
+export const IMAGE_URL = "https://providencehumancapital.com/medicalsapi/public/images"
 
 
 // export const API = "http://192.168.100.19:8080/medicalsapi2/public/api"
